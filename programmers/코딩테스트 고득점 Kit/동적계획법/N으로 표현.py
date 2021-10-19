8a0d5402e9c1967351fa6666012c611d533749e6
Sanghyun Hyun <73647404+lucid-aura@users.noreply.github.com>
2021-10-19 23:20:32 +0900

    Update N으로 표현.py
    알고리즘 풀이중

--- programmers/코딩테스트 고득점 Kit/동적계획법/N으로 표현.py
+++ programmers/코딩테스트 고득점 Kit/동적계획법/N으로 표현.py
@@ -1,49 +1,42 @@
 def solution(N, number):
-    def DFS(N, Ns, target, number, count):
-        #print(target, count)
-        if count > 8:
-            return -1
-
+    def DFS(Ns, target, number, count, exp):
         if target == number:
+            print(exp)
             return count
 
+        mins = []
         for i in Ns:
             # if target == 22 and i == 2:
             #     print(target, i)
-            count += Ns.index(i)+1
-            plus = DFS(i, Ns, target+i, number, count)
-            if plus == -1:
-                plus  = 9
-
-            minus = DFS(i, Ns, target-i, number, count)
-            if minus == -1:
-                minus  = 9
-
-            multi = DFS(i, Ns, target*i, number, count)
-            if multi == -1:
-                multi  = 9   
-
-            div = DFS(i, Ns, target//i, number, count)
-            if div == -1:
-                div  = 9
+            # if target == 22222 and i == 22:
+                #print(exp, target, i)
+            plus, minus, multi, div = 8,8,8,8
+            if count+Ns.index(i)+1 < 8:
+                plus = DFS(Ns, target+i, number, count+Ns.index(i)+1, exp + '+'+str(i))
+                minus = DFS(Ns, target-i, number, count+Ns.index(i)+1, exp +  '-'+str(i))
+                multi = DFS(Ns, target*i, number, count+Ns.index(i)+1, exp +  '*'+str(i))
+                div = DFS(Ns, target//i, number, count+Ns.index(i)+1, exp +  '/'+str(i))
+            mins.append(min(plus, minus, multi, div))
+        return min(mins)
         # if target == 22 and i == 2:
         #     print(target, min(plus, minus, multi, div))
-        if min(plus, minus, multi, div) == 9:
-            return -1
-        return min(plus, minus, multi, div)
+        #print(exp)
+       
 
-    if N < 3:
-        Ns = [int(str(N)*i) for i in range(1, 6)]
-    else:
-        Ns = [int(str(N)*i) for i in range(1, 5)]
+    # if N < 3:
+    #     Ns = [int(str(N)*i) for i in range(1, 6)]
+    # else:
+    #     Ns = [int(str(N)*i) for i in range(1, 5)]
+    Ns = [int(str(N)*i) for i in range(1, 8)]
     print(Ns)
 
     answer = []
     for i in Ns:
         count = Ns.index(i)+1
-        answer.append(DFS(i, Ns, i, number, count))
-        print(answer)
-    print(min(answer))
+        answer.append(DFS(Ns, i, number, count, str(i)))
+    print(answer)
+    if min(answer) == 8:
+        return -1
     return min(answer)
 
 
@@ -52,10 +45,14 @@ number = 12
 
 # 4
 
-N = 2
-number = 11
-# 3
+# N = 2
+# number = 11
 
+# N = 5
+# number = 31168
+# number = 100
+# 3
+N=2
+number=22244
 solution(N, number)
 
-print(22//2)
