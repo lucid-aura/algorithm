342595e988f5b629b6d6cacea323b3b05ed39475
Sanghyun Hyun <73647404+lucid-aura@users.noreply.github.com>
2021-10-19 17:54:25 +0900

    Update 11주차_아이템 줍기.py
    # 경로는 시계방향/반시계방향 중 하나이다.
    # rectangle list를 오름차순 정렬해 시작을 x의 최소로 한다.
    # 각각 사각형의 시계방향 둘레 경로를 route 리스트에 넣는다.
    # 모든 사각형들의 교점을 cross 리스트에 넣는다.
    # route 리스트들의 중복 값(점)이 교점임을 알 수 있다.
    # 현재 돌고 있는 둘레의 사각형을 rectangle_index로 설정한다.
    # 둘레 리스트의 현재 점 위치를 point_index로 설정한다.
    # 현재 점이 교점인 경우 반드시 사각형 번호가 변하게 된다.
    # 교점일 경우 현재 사각형과 점 index를 업데이트한다.
    # 교점이 아닐 경우 같은 사각형의 둘레를 따라 계속 이동한다.
    # 시계방향의 이동경로를 path리스트에 [x,y] 형태로 저장한다.
    # 한바퀴를 돌았을 경우 시계방향의 둘레 경로가 완성된다.
    # 시계 방향과 반시계 방향 중 최소값을 반환한다.
    # min(큰idx - 작은idx, len(path) - 큰idx, 작은idx)
    # 전자 : 시계방향 / 후자 : 반시계 방향

--- programmers/위클리챌린지/11주차_아이템 줍기.py
+++ programmers/위클리챌린지/11주차_아이템 줍기.py
@@ -1,134 +1,63 @@
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    table = [[0 for i in range(11)] for j in range(11)]
-    line = [[0 for i in range(11)] for j in range(11)]
-    
-    # for i in table:
-    #     print(i)
-    # print("###############################")
-    for rec in rectangle:
-        for i in range(rec[1], rec[3]+1):
-            for j in range(rec[0], rec[2]+1):
-                table[i][j] = 1
-                # if i == rec[1] or i == rec[3]:
-                #     point.append([i,j])
-                #     table[i][j] = 1
-                # elif j == rec[0] or j == rec[2]:
-                #     table[i][j] = 1
-                #     point.append([i,j])
-    for i in table:
-        print(i)
-    for i in range(10):
-        for j in range(10):
-            if table[i][j] == 0:
-                token = True
-                if i > 0:
-                    if table[i-1][j] == 0:
-                        token = False
-                if i < 10:
-                    if table[i+1][j] == 0:
-                        token = False
-                if j > 0:
-                    if table[i][j-1] == 0:
-                        token = False
-                if j < 10:
-                    if table[i][j+1] == 0:
-                        token = False
-                if token:
-                    table[i][j] = 1
+    def route_cw(rectangle): # 모든 방향은 시계방향을 도는 것으로 기준을 잡는다.
+        route = []
+        for i in range(rectangle[1], rectangle[3]):
+            route.append([rectangle[0], i])
+        for i in range(rectangle[0], rectangle[2]):
+            route.append([i, rectangle[3]])
+        for i in range(rectangle[3], rectangle[1], -1):
+            route.append([rectangle[2], i])
+        for i in range(rectangle[2], rectangle[0], -1):
+            route.append([i, rectangle[1]])
+        return route
 
-    for i in range(10):
-        for j in range(10):
-            if table[i][j] == 1:
-                token = True
-                if i > 0:
-                    if j > 0:
-                        if table[i-1][j-1] == 0:
-                            token = False
-                    if j < 10:
-                        if table[i-1][j+1] == 0:
-                            token = False
-                    if table[i-1][j] == 0:
-                        token = False
-                if i < 10:
-                    if j > 0:
-                        if table[i+1][j-1] == 0:
-                            token = False
-                    if j < 10:
-                        if table[i+1][j+1] == 0:
-                            token = False
-                    if table[i+1][j] == 0:
-                        token = False
-                if j > 0:
-                    if table[i][j-1] == 0:
-                        token = False
-                if j < 10:
-                    if table[i][j+1] == 0:
-                        token = False
-                if token:
-                    line[i][j] = 0
-                else:
-                    line[i][j] = 1
+    def cross_point(route): # 모든 사각형의 교점을 찾고 사각형 순서번호와 함께 cross 리스트에 저장한다.
+        cross = []
+        for i in range(len(route)-1):
+            for j in range(i+1, len(route)):
+                for k in route[i]:
+                    if k in route[j]:
+                        cross.append([k, i, j])
+        return cross
 
-    
-    # route = []
-    # for i in range(10):
-    #     for j in range(10):
-    #         if line[i][j] == 1:
-                
+    def check_cross(point, cross): # 교점이면 cross 원소를, 교점이 아니면 False를 반환한다.
+        for i in cross:
+            if point == i[0]:
+                return i
+        return False
 
-    # print("###############################")
-    # for i in table:
-    #         print(i)
-    # print("###############################")
-    # for i in line:
-    #         print(i)
-    #line[characterY][characterX] = 7
+    rectangle.sort()
 
-    def find_point(characterX, characterY, route):
+    # 각각의 사각형의 경로를 생성한다.
+    route = []
+    for i in rectangle:
+        route.append(route_cw(i))
 
-        point = []
-        if line[characterY-1][characterX] == 1 and [characterY-1,characterX] not in route:
-            point = [characterY-1,characterX]
-        elif line[characterY+1][characterX] == 1 and [characterY+1,characterX] not in route:  
-            point = [characterY+1,characterX]
-        elif line[characterY][characterX-1] == 1 and [characterY,characterX-1] not in route:
-            point = [characterY,characterX-1]
-        elif line[characterY][characterX+1] == 1 and [characterY,characterX+1] not in route:
-            point = [characterY,characterX+1]
-        return point
+    # 현재 위치한 사각형의 번호, 경로 위치 번호를 저장한다.
+    cross = cross_point(route)
+    rectangle_index = 0
+    point_index = 1
+    path = [route[rectangle_index][0]]
+    start = route[rectangle_index][0]
+    now = route[rectangle_index][point_index]
+    while start != now: # 한 바퀴 돌때까지 반복한다.
+        path.append(now)
+        check = check_cross(now, cross)
+        if check: # 교점이면 현재 사각형 index 변경
+            rectangle_index = check[(check.index(rectangle_index)) % 2 + 1]
+            print(rectangle_index)
+            point_index = (route[rectangle_index].index(now)+1) % len(route[rectangle_index])
+            now = route[rectangle_index][point_index]
+        else: # 교점이 아니면 그대로 다음 점으로 옮긴다.
+            point_index = (point_index+1) % len(route[rectangle_index])
+            now = route[rectangle_index][point_index]
 
-                
-    routeA = [[characterY, characterX]]
-    point = find_point(characterX, characterY, routeA)
-    routeA.append(point)
-    answerA = 1
-    xa = point[1]
-    ya = point[0]
+    character_index = path.index([characterX, characterY])
+    item_index = path.index([itemX, itemY])
 
-    routeA = [[characterY, characterX]]
-    point = find_point(characterX, characterY, routeA)
-    routeA.append(point)
-    point = find_point(characterX, characterY, routeA)
-    routeB = point
-    #print(routeB)
-    # xb = point[1]
-    # yb = point[0]
-    while [ya, xa] != [itemY, itemX]:
-        point = find_point(xa, ya, routeA)
-        routeA.append(point)
-        ya = point[0]
-        xa = point[1]
-        print(point, ya, xa)
-        answerA += 1
-        print(routeA)
-    print(answerA)
-        
-    for i in line:
-        print(i)
-    answer = 0
+    answer = min(len(path) - max(character_index, item_index) + min(character_index, item_index), max(character_index, item_index) - min(character_index, item_index))
+    print(answer)
     return answer
-
-# rectangle의 원소는 각 직사각형의 [좌측 하단 x, 좌측 하단 y, 우측 상단 x, 우측 상단 y] 좌표 형태입니다.
 
 rectangle = [[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]]
 characterX = 1
@@ -137,13 +66,36 @@ itemX = 7
 itemY = 8
 # 17
 
-rectangle = [[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]]
-characterX = 9
-characterY = 7
-itemX = 6
-itemY = 1
+# rectangle = [[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]]
+# characterX = 9
+# characterY = 7
+# itemX = 6
+# itemY = 1
 # 11
 
-
+rectangle = [[1,1,5,7]]
+characterX = 1
+characterY = 1
+itemX = 4
+itemY = 7
+# 9
 
 solution(rectangle, characterX, characterY, itemX, itemY)
+
+##########################################################
+# 경로는 시계방향/반시계방향 중 하나이다.
+# rectangle list를 오름차순 정렬해 시작을 x의 최소로 한다.
+# 각각 사각형의 시계방향 둘레 경로를 route 리스트에 넣는다.
+# 모든 사각형들의 교점을 cross 리스트에 넣는다.
+# route 리스트들의 중복 값(점)이 교점임을 알 수 있다.
+# 현재 돌고 있는 둘레의 사각형을 rectangle_index로 설정한다.
+# 둘레 리스트의 현재 점 위치를 point_index로 설정한다.
+# 현재 점이 교점인 경우 반드시 사각형 번호가 변하게 된다.
+# 교점일 경우 현재 사각형과 점 index를 업데이트한다.
+# 교점이 아닐 경우 같은 사각형의 둘레를 따라 계속 이동한다.
+# 시계방향의 이동경로를 path리스트에 [x,y] 형태로 저장한다.
+# 한바퀴를 돌았을 경우 시계방향의 둘레 경로가 완성된다.
+# 시계 방향과 반시계 방향 중 최소값을 반환한다.
+# min(큰idx - 작은idx, len(path) - 큰idx, 작은idx)
+# 전자 : 시계방향 / 후자 : 반시계 방향
+##########################################################
