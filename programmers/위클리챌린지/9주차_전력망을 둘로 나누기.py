f54e40d48ab08f65c60220e9ea1eede7ded5b32f
Sanghyun Hyun <73647404+lucid-aura@users.noreply.github.com>
2021-10-23 20:05:22 +0900

    Update 9주차_전력망을 둘로 나누기.py
    ########################################################
    # 전력망을 하나씩 끊어보면서 차가 최소인 값을 찾는다.
    # 다음 노드가 없을때까지 한 노드에서 다른 노드로 가는 경로가 있을 경우 방문 노드에 추가한다.
    # 더이상 방문하지 않은 노드가 없을 경우 길이가 변하지 않는다. - 탈출 조건 (노드 추가가 되지 않았기 때문에)
    # 방문한 노드 개수와 전체 노드의 차이가 전력망을 하나 끊었을 때의 두 트리의 노드 개수의 차이다.
    ########################################################

--- programmers/위클리챌린지/9주차_전력망을 둘로 나누기.py
+++ programmers/위클리챌린지/9주차_전력망을 둘로 나누기.py
@@ -19,24 +19,13 @@ def solution(n, wires):
         answer = min(answer, abs(n- 2*length))
     return answer
 
-
-# def solution(n, wires):
-#     def match(wire):
-#         node = {wire[0][0], wire[0][1]}
-#         for i in wire:
-#             if i[0] in node or i[1] in node:
-#                 node.add(i[0])
-#                 node.add(i[1])
-#         return len(node)
-    
-#     answer = len(wires)
-#     if answer == 1:
-#         return 0
-#     for i in range(len(wires)):
-#         wire = wires[0:i] + wires[i+1:n]
-#         print(wire, match(wire),n - match(wire)*2)
-#         answer = min(answer, abs(n - match(wire)*2))
-
 n = 9
 wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
 solution(n, wires)
+
+########################################################
+# 전력망을 하나씩 끊어보면서 차가 최소인 값을 찾는다.
+# 다음 노드가 없을때까지 한 노드에서 다른 노드로 가는 경로가 있을 경우 방문 노드에 추가한다.
+# 더이상 방문하지 않은 노드가 없을 경우 길이가 변하지 않는다. - 탈출 조건 (노드 추가가 되지 않았기 때문에)
+# 방문한 노드 개수와 전체 노드의 차이가 전력망을 하나 끊었을 때의 두 트리의 노드 개수의 차이다.
+########################################################
