f0d57418a43f1d71b04f0f7083919278f6319ec3
Sanghyun Hyun <73647404+lucid-aura@users.noreply.github.com>
2021-10-26 19:12:56 +0900

    단체 사진 / 카카오프렌즈 컬러링북 프로젝트 추가

---
+++ c++/단체사진/단체사진/단체사진.cpp
@@ -0,0 +1,87 @@
+﻿// 단체사진.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool except(string line, vector<string> data) {
+    for (int i = 0; i < data.size(); i++) {
+        char a = data[i][0];
+        char b = data[i][2];
+        char exp = data[i][3];
+        char c = data[i][4];
+        int idx_a = line.find(a);
+        int idx_b = line.find(b);
+        if (idx_a != string::npos && idx_b != string::npos) {
+            int int_a = a - '0';
+            int int_b = b - '0';
+            int int_c = c - '0';
+            if (exp == '<') {
+                if (abs(idx_a - idx_b) >= int_c + 1) {
+                    return true;
+                }
+            }
+            else if (exp == '>') {
+                if (abs(idx_a - idx_b) <= int_c + 1) {
+                    return true;
+                }
+            }
+            else {
+                if (abs(idx_a - idx_b) != 1) {
+                    return true;
+                }
+            }
+        }
+        //cout << a << " " << b << " " << exp << " " << c << endl;
+    }
+    return false;
+}
+int DFS(vector<char>& name, string line, vector<string> &lines, vector<string> &data) {
+    if (line.size() == 8 && (find(lines.begin(), lines.end(), line) == lines.end())) {
+        lines.push_back(line);
+        //cout << line << endl;
+        return 0;
+    }
+    //cout << line << endl;
+    for (int i = 0; i < 8; i++) {
+        int idx = line.find(name[i]);
+        
+        if (idx == string::npos) {
+            string new_line = line + name[i];
+            if (!except(new_line, data)) {
+                DFS(name, new_line, lines, data);
+            }
+        }
+    }
+    return 0;
+}
+
+int solution(int n, vector<string> data) {
+    vector<string> friends = { "A", "C", "F" , "J", "M", "N" , "R", "T"};
+    vector<char> name = { 'A','C','F','J','M','N','R','T' };
+    //string test = "ACFJMNRTSP";
+    string line = "";
+    vector<string> lines;
+    for (int i = 0; i < 8; i++) {
+        line = friends[i];
+        DFS(name, line, lines, data);
+    }
+    cout << lines.size() << endl;
+    
+
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    vector<string> data = { "N~F=0", "R~T>2" };
+    int n = 2;
+    // 3048
+    solution(n, data);
+    //except("a", data);
+}
---
+++ c++/카카오프렌즈 컬러링북/카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
@@ -0,0 +1,74 @@
+﻿// 카카오프렌즈 컬러링북.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print(vector<vector<int>> picture, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << picture[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "############################" << endl;
+}
+int DFS(vector<vector<int>> &picture, int n, int m,  int i, int j, int color, int &area) {
+    if (picture[i][j] != color) {
+        return 0;
+    }
+    area++;
+    picture[i][j] = 0;
+    if (i-1 > 0) {
+        DFS(picture, n, m, i - 1, j, color, area);
+    }
+    if (i+1 < m) {
+        DFS(picture, n, m, i + 1, j, color, area);
+    }
+    if (j-1 > 0) {
+        DFS(picture, n, m, i , j - 1, color, area);
+    }
+    if (j+1 < n) {
+        DFS(picture, n, m, i, j + 1, color, area);
+    }
+    return 0;
+}
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    int number_of_area = 0;
+    int max_size_of_one_area = 0;
+
+    int area = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (picture[i][j] > 0) {
+                DFS(picture, n, m, i, j, picture[i][j], area);
+                number_of_area++;
+                if (area > max_size_of_one_area) {
+                    max_size_of_one_area = area;
+                }
+                area = 0;            }
+        }
+    }
+    vector<int> answer(2);
+    answer[0] = number_of_area;
+    answer[1] = max_size_of_one_area;
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    vector<vector<int>> picture = { {1,1,1,0},{1,2,2,0},{1,0,0,1},{0,0,0,1},{0,0,0,3},{0,0,0,3} };
+    int m = 6;
+    int n = 4;
+
+    m = 13;
+    n = 16;
+    picture = { {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, {0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 0}, {0, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0} };
+    vector<int> answer(2);
+    answer = solution(m, n, picture);
+    cout << answer[0] << " " << answer[1] << endl;
+}
